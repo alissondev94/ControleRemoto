92a803f39817e821256d2150bb162c956703748a
Alisson <Alissonpx25@gmail.com>
2024-09-27 23:05:16 -0300

    Study Interface

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        ControleRemoto c1 = new ControleRemoto();
+        c1.ligar();
+        c1.abrirMenu();
+
+    }
+}
